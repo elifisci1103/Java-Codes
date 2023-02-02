2d5cad74e260360563fe63c37a6d97665d739ccc
Elifisci1103@gmail.com <you@example.com>
2023-02-02 23:24:51 +0300

    v1

---
+++ src/day06/NestedTernary.java
@@ -0,0 +1,18 @@
+package day06;
+
+public class NestedTernary {
+    public static void main(String[] args) {
+        // Kullanicidan bir tamsayi alin.
+        //  Sayi pozitifse, cift sayi veya cift sayi degil seceneklerinden uygun olani yazdirin
+        //  Sayi pozitif degilse, 3 basamakli veya 3 basamakli degil seceneklerinden uygun olani yazdirin
+        int sayi= 28;
+        // System.out.println(sayi>0 ? "sayi pozitif" : "sayi pozitif degil");
+        System.out.println(sayi>0
+                ?
+                sayi%2==0 ? "cift sayi" : "tek sayi"
+                :
+                -100>=sayi && sayi>=-999 ? "3 basamakli" : "# basamakli degil"
+        );
+        System.out.println(sayi>0 ? sayi%2==0 ? "cift sayi" : "tek sayi" :-100>=sayi && sayi>=-999 ? "3 basamakli" : "# basamakli degil");
+    }
+}
