0386b81b8420583e9b6f5d220d26cc38fcc1eab4
Elifisci1103@gmail.com <you@example.com>
2023-02-02 23:26:29 +0300

    v2

---
+++ src/day06/NestedTernary2.java
@@ -0,0 +1,18 @@
+package day06;
+
+public class NestedTernary2 {
+    public static void main(String[] args) {
+        // input olarak verilen sayiyi kontrol edip
+        // sayi cift ise "cift sayi"
+        // degilse "tek sayi" yazdirin
+        int input=44;
+        // if else ile yapalim
+        if (input%2==0){
+            System.out.println("cift sayi");
+        } else {
+            System.out.println("tek sayi");
+        }
+        // ternary ile yapalim
+        System.out.println(input%2==0 ? "Cift sayi" : "Tek sayi");
+    }
+}
